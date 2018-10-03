d876835d3c060f2cc47bd0a194dc27f776b5f767
Anti <anti@antis-imac-pro.local>
2018-10-03 14:15:46 -0700

    did some color changing

--- ConcentrationLecture/ConcentrationViewController.swift
+++ ConcentrationLecture/ConcentrationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
 
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
@@ -16,13 +16,23 @@ class ViewController: UIViewController {
         return (cardButtons.count + 1) / 2
     }
     
-    private(set) var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
+    private(set) var flipCount = 0 { didSet { updateFlipCountLabel() } }
+        
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedString.Key:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(
+            string: "Flips: \(flipCount)",
+            attributes: attributes
+        )
+        flipCountLabel.attributedText = attributedString
     }
     
-    @IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet { updateFlipCountLabel() }
+    }
     
     @IBOutlet private var cardButtons: [UIButton]!
     
@@ -42,16 +52,24 @@ class ViewController: UIViewController {
             let card = game.cards[index]
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
             }
         }
     }
     
-    private var emojiChoices = ["🎃", "👻", "🦊", "🐨", "🐱", "🐭", "🐙", "🦑", "🍞"]
+    var theme: String? {
+        didSet {
+            emojiChoicesLecture = theme ?? ""
+            emoji = [:]
+            updateViewFromModel()
+        }
+    }
     
+    private var emojiChoicesLecture = "🎃👻🦊🐨🐱🐭🐙🦑🍞"
+    private var emojiChoices = ["🎃", "👻", "🦊", "🐨", "🐱", "🐭", "🐙", "🦑", "🍞"]
     private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
